4eab0c0a4afd42e7c81137e297b5e9aac0e1fb44
rui zhou <wirelesser@hotmail.com>
2018-03-17 23:57:06 +0800

    350

--- ojcpp/leetcode/237_deletenodeinlinkedlist.cpp
+++ ojcpp/leetcode/237_deletenodeinlinkedlist.cpp
@@ -19,6 +19,9 @@ Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node wi
  */
 
 #include <codech/codech_def.h>
+#include <codech/algo_common.h>
+
+using namespace CODECH;
 
 class Solution {
 public:
