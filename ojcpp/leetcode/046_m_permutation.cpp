4bac038543536e490f96a403263e2f3413fbc11d
rui zhou <wirelesser@hotmail.com>
2019-03-05 23:54:58 +0800

    047

--- ojcpp/leetcode/046_m_permutation.cpp
+++ ojcpp/leetcode/046_m_permutation.cpp
@@ -38,11 +38,17 @@ namespace permutation {
     // 1.使用next_permutation
     // 2. HHT offer p172, 递归的做法,把第一个字符和后面所有字符交换(包括自身)，然后再递归(start++)求后面的部分的和其第一个字符的交换
     // 当start到达尾部的时候退出递归，添加结果
+    // 1 2 3->1 2 3 -> 1 2 3
+    //          |-> 1 3 2 --> 1 3 2
+    //
     class Solution1 {
     public:
         void dfs(int start,int end, vector<int> comb,vector<vector<int>> &ret) {
-            if (start==end-1)
+            if (start==end-1) {
                 ret.emplace_back(comb);
+                cout<<PRINT_VEC(std::move(comb))<<endl;
+            }
+
             for (int i=start;i<end;i++) {
                 swap(comb[start],comb[i]);
                 dfs(start+1,end,comb, ret);
--- ojcpp/leetcode/047_m_permutation2.cpp
+++ ojcpp/leetcode/047_m_permutation2.cpp
@@ -18,15 +18,65 @@ Output:
  */
 #include <codech/codech_def.h>
 using namespace std;
+using namespace CODECH;
 
+
+//s实在难以判断退出条件
 class Solution {
 public:
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
+    void dfs(int start, int end, vector<int> comb,vector<vector<int>> &ret) {
+        if (start==end-1) {
+            ret.emplace_back(comb);
+            cout<<PRINT_VEC(std::move(comb))<<endl;
+        }
+        unordered_set<int> visited;
+        for (int i=start;i<end;i++) {
+            if (visited.count(comb[i])==0) {
+                swap(comb[start],comb[i]);
+                visited.emplace(comb[i]);
+                dfs(start+1,end,comb, ret);
+            }
 
+
+        }
+    }
+
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ret;
+        dfs(0,nums.size(),nums, ret);
+        return ret;
     }
 };
 
+
+
+// 排列问题且唯一问题，可以用hashmap，从里面挑数字
 DEFINE_CODE_TEST(047_permutation2)
 {
+    Solution obj;
+    {
+        vector<int> nums{2,2,1,1};
+        VERIFY_CASE(PRINT_VVEC(obj.permuteUnique(nums)),"3 3 0 3 3 3 3 0 3 0 3 3 0 3 3 3");
+    }
+    {
+        vector<int> nums{3,3,0,3};
+
+    }
+    {
+        vector<int> nums{1,1,2};
+        VERIFY_CASE(PRINT_VVEC(obj.permuteUnique(nums)),"1 1 2 1 2 1 2 1 1");
+    }
+    {
+        vector<int> nums{1,1,1};
+        VERIFY_CASE(PRINT_VVEC(obj.permuteUnique(nums)),"1 1 1");
+    }
+
+    {
+        vector<int> nums{1,1,1,2};
+        VERIFY_CASE(PRINT_VVEC(obj.permuteUnique(nums)),"1 1 1 2 1 1 2 1 1 2 1 1 2 1 1 1");
+    }
+
+
+
 
 }
