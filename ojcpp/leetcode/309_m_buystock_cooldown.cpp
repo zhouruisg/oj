9ab87e600a53acb69a32d1e651d0414d450fec57
zhourui <wirelesser@hotmail.com>
2019-04-01 22:40:12 +0800

    virtu financial

--- ojcpp/leetcode/309_m_buystock_cooldown.cpp
+++ ojcpp/leetcode/309_m_buystock_cooldown.cpp
@@ -17,7 +17,38 @@ Explanation: transactions = [buy, sell, cooldown, buy, sell]
 
  -> 难以判断触发点？1,2,3,0,2,5
  backtracking？
- 
+
+ int maxProfit(int* prices, int pricesSize) {
+    if(pricesSize<2){
+        return 0;
+    }
+    int* buy=(int*)calloc((pricesSize+1),sizeof(int));
+    int* sell=(int*)calloc((pricesSize+1),sizeof(int));
+    buy[1]=-prices[0];
+    for(int i=2;i<=pricesSize;i++){
+        buy[i]=max(sell[i-2]-prices[i-1],buy[i-1]);
+        sell[i]=max(sell[i-1],buy[i-1]+prices[i-1]);
+    }
+    return sell[pricesSize];
+}
+
+ var maxProfit = function(prices) {
+    if (prices.length <= 1) return 0
+    var dp = []
+    for (let i = 0; i < prices.length; i++) {
+        dp[i] = []
+    }
+
+    dp[0][0] = 0
+    dp[0][1] = -prices[0]
+    dp[0][2] = Number.MIN_VALUE
+    for (let i = 1; i < prices.length; i++)  {
+        dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][2]) //rest
+        dp[i][1] = Math.max(dp[i - 1][0] - prices[i], dp[i - 1][1]) //buy status
+        dp[i][2] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]) //sell status
+    }
+    return Math.max(dp[prices.length - 1][0], dp[prices.length - 1][2])
+}
  */
 
 #include <codech/codech_def.h>
@@ -28,13 +59,25 @@ namespace {
     public:
         int maxProfit(vector<int>& prices) {
             int total=0;
+            int n=prices.size();
+            vector<int> buy(n,0),sell(n,0),rest(n,0);
+            buy[0]=-prices[0];
             for (int i=1;i<prices.size();i++) {
-                if (prices[i]>prices[i-1]) {
-                    total+=prices[i]-prices[i-1];
-                } else {
-
-                }
+                buy[i]=max(rest[i-1]-prices[i],buy[i-1]);  // 前一天是持有状态，前一天是冷静期过后买入,谁更好
+                sell[i]=max(buy[i-1]+prices[i],sell[i-1]); // 比较前一天sell和今天sell谁更好
+                rest[i]=max({buy[i-1],sell[i-1],rest[i-1]});
             }
+            return max(rest[n-1],sell[n-1]);
         }
     };
+}
+
+
+DEFINE_CODE_TEST(309_buystock_cooldown)
+{
+    Solution obj;
+    {
+        vector<int> nums{1,2,3,0,2};
+        VERIFY_CASE(obj.maxProfit(nums),3);
+    }
 }
