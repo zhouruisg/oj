97d3925516e1b44f87756ef7f9d6a760a21911ea
zhourui <wirelesser@hotmail.com>
2019-05-17 23:10:16 +0800

    add exception

---
+++ vc/cpp11/008_operator_new.cpp
@@ -0,0 +1,106 @@
+//
+// Created by rui.zhou on 5/17/2019.
+//
+
+#include <codech/codech_def.h>
+using namespace std;
+namespace {
+    // 一个基本的operator new 例子
+    class A    {
+    public:
+        A() {
+            std::cout<<"call A constructor"<<std::endl;
+        }
+
+        ~A() {
+            std::cout<<"call A destructor"<<std::endl;
+        }
+        void* operator new(size_t size) {
+            std::cout<<"call A::operator new, size="<< size << std::endl;
+            return malloc(size);
+        }
+
+        void* operator new(size_t size, const std::nothrow_t& nothrow_value)  {
+            std::cout<<"call A::operator new nothrow"<<std::endl;
+            return malloc(size);
+        }
+    };
+
+    void test_operator_new() {
+        A* p1 = new A;
+        cout << p1 << endl;
+        delete p1;
+
+        A* p2 = new(std::nothrow) A;
+        cout << p2 << endl;
+        delete p2;
+    }
+
+    // EFC 049 ---------------------------------------------------
+    // 使用CRTP curiously recursive template
+    template<typename T> // “mixin-style” base class for
+    class NewHandlerSupport { // class-specific set_new_handler
+    public: // support
+        static std::new_handler set_new_handler(std::new_handler p) throw() {
+            std::new_handler oldHandler = currentHandler;
+            currentHandler = p;
+            return oldHandler;
+        }
+
+        static void* operator new(std::size_t size) throw(std::bad_alloc) {
+            size = size * 1000;
+            cout << "operator new size=" << size << endl;
+            for (;;) {
+                if (void *p=malloc(size)) return p;
+                new_handler globalHandler = set_new_handler(0);
+                set_new_handler(globalHandler);
+                if (globalHandler)
+                    (*globalHandler)();
+                else
+                    throw std::bad_alloc();
+            }
+            std::new_handler h = std::set_new_handler(currentHandler);
+            //return ::operator new(size);
+        }
+        // other versions of op. new —
+        // see Item 52
+    private:
+        static std::new_handler currentHandler;
+    };
+
+    // this initializes each currentHandler to null
+    template<typename T>
+    std::new_handler NewHandlerSupport<T>::currentHandler = 0;
+
+    class B :public NewHandlerSupport<B> {
+    public:
+        B() {
+            cout << "B ctor" << endl;
+        }
+    private:
+        int buf[1000*1000*1000]; // 1G
+    };
+
+    void test_crtp() {
+        // 并不能捕捉exception
+        // 直接声明对象的方法也不会去call operator new
+//        try {
+//            B objb1;
+//        } catch (...) {
+//            cout << "caught error objb" << endl;
+//        }
+
+        B *objb = nullptr;
+        try {
+            objb = new B();
+        } catch (...) {
+            cout << "caught error objb=" << objb << endl;
+        }
+    }
+}
+
+DEFINE_CODE_TEST(008_operator_new)
+{
+    //test_operator_new();
+    test_crtp();
+}
--- vc/cpp11/130_perfectforward.cpp
+++ vc/cpp11/130_perfectforward.cpp
@@ -5,35 +5,72 @@
 #include <codech/codech_def.h>
 #include <functional>
 using namespace std;
-
+//perfect forward failure case
 namespace {
-    void f(int (*pf)(int)) {
-
+    // ---------------------------------------------
+    // 参数为initializer_list的时候
+    void f1(const vector<int>&v) {
+        cout << v.size() << endl;
     }
+    template<typename ...T>
+    void fwd1(T&& ... param){
+        f1(std::forward<T>(param)...);
+    }
+
+    void test_initializer_list() {
+        //f1({1,2,3,4});  //good
+        auto param = {1,2,3,4};
+        cout << typeid(param).name() << endl;
+        fwd1(param);
+    }
+
+    // ---------------------------------------------
+    // 参数为static const data member
+    void f2(std::size_t val) {
+        cout << val << endl;
+    }
+    template<typename ...T>
+    void fwd2(T&& ... param){
+        f2(std::forward<T>(param)...);
+    }
+
+    class Widget {
+    public:
+        static const std::size_t MinVals = 28;
+    };
+    void test_static_member() {
+        fwd2(Widget::MinVals);
+        //f2(Widget::MinVals);
+    }
+
+    // ---------------------------------------------
+    //重载函数名字和模板名字
+    void f(int (*pf)(int)) { }
+    int processVal(int value) { return 0;}
+    int processVal(int value, int priority) {return 0;}
+
     //void f(int pf(int)) {  }
     template<typename T>
     void fwd(T&& param)          // 接受参数
     {
         f(std::forward<T>(param));      // 转发到f
     }
-    int processVal(int value) { return 0;}
-    int processVal(int value, int priority) {return 0;}
 
     void testfuncptr()
     {
         f(processVal);  //f(int (*pf)(int))
         //fwd(processVal);  // 不知道匹配哪个
-
         using ProcessFuncType = int (*)(int);
         ProcessFuncType processValPtr = processVal;
         fwd(processValPtr);  // 指明类型
-
         //auto bindfunc = bind(processVal,placeholders::_1,placeholders::_1); // bind也不行，不知道bind哪个
         //fwd();
     }
 }
 DEFINE_CODE_TEST(emc_130_perfect_forward)
 {
-    testfuncptr();
+    //testfuncptr();
+    //test_initializer_list();
+    test_static_member();
 }
 
