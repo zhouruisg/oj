5f4eb640b10e21c519de7ccfd6959226ae2fe0b6
Zhou Rui <wirelesser@hotmail.com>
2019-09-05 22:30:11 +0800

    692

--- ojcpp/leetcode/400/450_m_delnodeinbst.cpp
+++ ojcpp/leetcode/400/450_m_delnodeinbst.cpp
@@ -176,7 +176,7 @@ DEFINE_CODE_TEST(450_delnodeinbst)
     {
         TreeNode *root = LCREATE_TREENODE({2,0,33,null,1,25,40,null,null,11,31,34,45,10,18,29,32,null,36,43,46,4,null,12,24,26,30,null,null,35,39,42,44,null,48,3,9,null,14,22,null,null,27,null,null,null,null,38,null,41,null,null,null,47,49,null,null,5,null,13,15,21,23,null,28,37,null,null,null,null,null,null,null,null,8,null,null,null,17,19,null,null,null,null,null,null,null,7,null,16,null,null,20,6});
         //TREE_BYLEVEL(root);
-        VERIFY_CASE(TREE_BYLEVEL(obj.deleteNode0(root, 33)),"2,0,34,null,1,25,40,null,null,11,31,36,45,10,18,29,32,35,39,43,46,4,null,12,24,26,30,null,null,null,null,38,null,42,44,null,48,3,9,null,14,22,null,null,27,null,null,37,null,41,null,null,null,47,49,null,null,5,null,13,15,21,23,null,28,null,null,null,null,null,null,null,null,null,8,null,null,null,17,19,null,null,null,null,null,7,null,16,null,null,20,null,null,null,null,null,null,");
+        VERIFY_CASE(TREE_BYLEVEL(obj.deleteNode(root, 33)),"2,0,34,null,1,25,40,null,null,11,31,36,45,10,18,29,32,35,39,43,46,4,null,12,24,26,30,null,null,null,null,38,null,42,44,null,48,3,9,null,14,22,null,null,27,null,null,37,null,41,null,null,null,47,49,null,null,5,null,13,15,21,23,null,28,null,null,null,null,null,null,null,null,null,8,null,null,null,17,19,null,null,null,null,null,7,null,16,null,null,20,null,null,null,null,null,null,");
     }
     /*
     {
--- ojcpp/leetcode/692_m_topfrequentwords.cpp
+++ ojcpp/leetcode/692_m_topfrequentwords.cpp
@@ -25,18 +25,60 @@ Try to solve it in O(n log k) time and O(n) extra space.
 
 
 #include <codech/codech_def.h>
-
+using namespace CODECH;
 using namespace std;
 
 namespace {
     class Solution {
     public:
         vector<string> topKFrequent(vector<string>& words, int k) {
-            priority_queue<string> pq;
+            // default is max heap(less<>), we want a min heap
+            auto comp = [](pair<string,int> &a, pair<string,int>&b){
+                if (a.second!=b.second)
+                    return a.second > b.second;
+                else {
+                    return (bool)(a.first.compare(b.first)<=0);
+                }
+            };
+
+            priority_queue<pair<string, int>, vector<pair<string,int>>,
+                    decltype(comp)> pq(comp);
+
             unordered_map<string,int> map;
+            vector<string> ret;
             for (auto &&it:words) {
-                map[]
+                map[it]+=1;
             }
+
+            for (auto &&it : map) {
+                pq.push(make_pair(it.first,it.second));
+                if (pq.size() > k) {
+                    pq.pop();
+                }
+            }
+
+            while (!pq.empty()) {
+                ret.insert(ret.begin(),pq.top().first);pq.pop();
+            }
+            return ret;
         }
     };
+}
+
+DEFINE_CODE_TEST(692_topfrequentwords)
+{
+    Solution obj;
+    {
+        vector<string> words = {"the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"};
+        vector<string> exp = {"the", "is", "sunny", "day"};
+        VERIFY_CASE(obj.topKFrequent(words,4) == exp,true);
+        cout << PRINT_STRVEC(obj.topKFrequent(words,4)) << endl;
+    }
+    {
+        vector<string> words = {"i", "love", "leetcode", "i", "love", "coding"};
+        vector<string> exp = {"i", "love"};
+        VERIFY_CASE(obj.topKFrequent(words,2) == exp,true);
+        cout << PRINT_STRVEC(obj.topKFrequent(words,2)) << endl;
+    }
+
 }
