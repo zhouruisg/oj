391b12bf19ccefd5890cabcb0fe8743471ffed44
rui zhou <wirelesser@hotmail.com>
2019-09-18 23:50:30 +0800

    621

--- ojcpp/leetcode/1143_m_longestcommonsubsequence.cpp
+++ ojcpp/leetcode/1143_m_longestcommonsubsequence.cpp
@@ -44,13 +44,14 @@ namespace {
     class Solution {
     public:
         int longestCommonSubsequence(string text1, string text2) {
-            int dp[1001][1001] = {0};
-            for (int i=1; i <text1.length();i++) {
-                for (int j=1;j<text2.length();j++) {
-                    dp[i][j] = text1[i]==text2[j]?dp[i-1][j-1]+1:max(dp[i-1][j], dp[i][j-1]);
-                }
-            }
-            return dp[text1.length()-1][]
+//            int dp[1001][1001] = {0};
+//            for (int i=1; i <text1.length();i++) {
+//                for (int j=1;j<text2.length();j++) {
+//                    dp[i][j] = text1[i]==text2[j]?dp[i-1][j-1]+1:max(dp[i-1][j], dp[i][j-1]);
+//                }
+//            }
+//            return dp[text1.length()-1][]
+return 0;
         }
     };
 }
