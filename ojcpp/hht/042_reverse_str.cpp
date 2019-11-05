aa90ca5fcdb29f2da6bb65cfffcaae1191fa8a39
rui zhou <wirelesser@hotmail.com>
2019-11-05 23:46:14 +0800

    Hht 042

--- ojcpp/hht/042_reverse_str.cpp
+++ ojcpp/hht/042_reverse_str.cpp
@@ -5,8 +5,8 @@
 /*
  反转字符串
 
- 思路
-先从小的开始加，一但超过了，就把最小的减掉
+ lc557
+
  */
 
 
@@ -14,42 +14,39 @@
 using namespace std;
 using namespace CODECH;
 
+
+
 namespace {
     class Solution {
     public:
-        vector <vector<int>> FindContinuousSequence(int sum) {
-            vector<vector<int>> ret;
-            int cursum = 1;
-            int begin=1,end=1;
-            while ((cursum!=sum || begin<=end) && end <= (sum+1)/2) {
-                if (cursum+end+1 < sum) {
-                    end++;
-                    cursum+=end;
-                } else if (cursum+end+1 > sum) {
-                    cursum-=begin;
-                    begin+=1;
-                } else {
-                    vector<int> ans;
-                    for (int i=begin;i<=end+1;i++) {
-                        ans.push_back(i);
-                    }
-                    ret.emplace_back(ans);
-                    cursum-=begin;
-                    begin+=1;
-                }
-            }
-            return ret;
-        }
+        string reverseWords(string s) {
+            string ans;
+            stack<string> st;
+            stringstream ss(s);
+            string tmp;
 
+            while (getline(ss,tmp,' ')) {
+                st.push(tmp);
+            }
+
+            while (!st.empty()) {
+                ans+=st.top();
+                st.pop();
+                if (!st.empty())
+                    ans+=' ';
+            }
+            return ans;
+        }
     };
 }
 
-DEFINE_CODE_TEST(041_continuous_seq)
+
+DEFINE_CODE_TEST(042_reverse_str)
 {
     Solution obj;
     {
-        vector<vector<int>> exp{{1,2,3,4,5},{4,5,6},{7,8}};
-        VERIFY_CASE(VERIFY_VV(obj.FindContinuousSequence(15),exp),true);
+        VERIFY_CASE(obj.reverseWords("I am a student."),"student. a am I");
+
     }
 
 }
